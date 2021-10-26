1c9c86773bee79f3cd5bab3490fafcfa44bf358e
Ryan Lee <ryanyinlee@protonmail.com>
2021-10-26 12:02:35 -0700

    Update script.js

--- Lab02/script.js
+++ Lab02/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let someData = prompt("Pick a number");
+ // let someData = prompt("Pick a number");
 someData = parseInt(someData);
 let testString = true;
 
@@ -12,4 +12,18 @@ console.log("This result is a " + result);
 
 if (someData == 42) {
     alert("ya this was 42");
+}
+
+switch (expr) {
+case 'Oranges':
+    console.log('Oranges are $0.59 per pound.');
+    break;
+case 'Mangoes':
+    console.log('Mangoes are $1.29 per pound.');
+    break;
+default:
+    console.log('Ya no fruit today.');
+
+
+
 }
